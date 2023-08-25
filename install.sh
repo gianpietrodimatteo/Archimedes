1d6bdf864de5641abfe6bbc117e9282a4001cdb6
Gianpietro Di Matteo <gianpietro.dimatteo@gmail.com>
2023-08-25 18:18:05 -0300

    Initial installation setup

---
+++ install.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# # Starts by installing the system's dependencies
+# sudo pacman -Syu git firefox code rclone libreoffice networkmanager nm-connection-editor network-manager-applet kitty i3 git nano vim pacman-contrib xorg xfce4 xfce4-goodies lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings pulseaudio pulseaudio-bluetooth pavucontrol bluez ack nitrogen tmux autoconf automake gcc github-cli bash-completion gvfs gvfs-afc ntfs-3g picom ttf-fira-code
+
+# sudo systemctl enable lightdm.service NetworkManager.service
+
+# mkdir ~/builds
+
+# cd ~/builds
+
+# sudo pacman -S --needed base-devel
+# git clone https://aur.archlinux.org/paru.git
+# cd paru
+# makepkg -si
+
+# paru i3ipc-glib
+# paru i3-workspaces
+
+# remove xfwm4 and xdesktop from startup
+# add i3 to startup
+#
+# add i3-workspaces to panel
+# remove workspace viewer from panel
+#
+# fire up nitrogen, choose wallpaper
+# change theme to adwaita dark
+#
+# setup github
+#
+#
+#
+#
+# ln confs
+# kitty
+# i3
+# rclone setup
+
+# xmodmap
+#
+# Shouldnt we modularize all those basic configs?
+# VSCODE kitty i3
+# i3 is being used again
+# although I needed to recreate its defaults again
+# what about github, huh?
+#
+# better to modularize everyone. . . is it? should be
+#
+
+
+# mkdir ~/Workspace
+# cd ~/Workspace
+# git clone https://github.com/gianpietrodimatteo/Bashtion.git
+# cd Bashtion;./install.sh
+
+# cd ~/Workspace
+# git clone https://github.com/gianpietrodimatteo/Vimcent.git
+# cd Vimcent;./install.sh
+
+# rm ~/.config/i3/config
+# ln -sv "$(pwd)/i3-config" ~/.config/i3/config
+# rm ~/.Xmodmap
+# ln -sv "$(pwd)/Xmodmap" ~/.Xmodmap
+
+# reboot
+
+
